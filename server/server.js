f46c43001e4c553af23e160f2b849c6c3f5aea2b
sumanjali312 <chinnamgarisumanjali13@gmail.com>
2025-09-04 17:18:54 +0530

    model import fix

--- server/server.js
+++ server/server.js
@@ -6,19 +6,18 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
+
+// Connect to database
 await connectDB();
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware);
 
+// Routes
 app.get("/", (req, res) => res.send("Server is Live!"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-export default app; // DO NOT use app.listen() here
+// Export app for Vercel (do not listen here)
+export default app;
